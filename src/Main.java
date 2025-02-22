2f29ccedf319796650eb7cbc25bb6479547d403d
rohit <rohitsrivastav756@gmail.com>
2025-02-22 14:19:10 +0530

    I have change in main SOP change

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println("hi I learn git");
         Demo1 d= new Demo1();
         d.first();
+        System.out.println("hi I am use compare and pull request");
         Demo dd= new Demo();
         dd.first();
         dd.show();
