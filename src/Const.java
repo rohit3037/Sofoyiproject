b6a1cf4ec316dff636be308af2801004219c3e89
rohit <rohitsrivastav756@gmail.com>
2025-02-23 14:46:04 +0530

    I have add new Const class and some change in main method

---
+++ src/Const.java
@@ -0,0 +1,10 @@
+public class Const {
+    public void test()
+    {
+        System.out.println("+++++++++++hi I am add new class like NewInnher+++++");
+    }
+    public  void RR(int a)
+    {
+        System.out.println("Add new class constractor ???????"+a);
+    }
+}
